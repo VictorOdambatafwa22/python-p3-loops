d3ec9ad64975d2ecad95ca903b9db93a71c040b7
victor odambatafwa <omondivictoras@gmail.com>
2023-08-16 11:50:31 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+
+  counter=10
+  while counter>0:
+    print(counter)
+    counter-=1
+  print("Happy New Year!")
+
+happy_new_year()
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   x = int_list
+   squared = [i**2 for i in x]   
+   return squared
+
+square_integers([1, 2, 3, 4,5])
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+ for num in range(1, 101):
+  if (num % 3 == 0 and num % 5 == 0):
+    print("FizzBuzz")
+  elif num % 3 == 0: 
+    print("Fizz")
+  elif num % 5 == 0:
+     print("Buzz")
+  else:
+    print(num)
+fizzbuzz()
+
+
